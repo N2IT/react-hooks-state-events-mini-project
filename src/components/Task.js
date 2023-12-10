5faa03e8d347e149f2853ab2e527d1ed5a5ff4db
Tony Eder <tonyeder11@gmail.com>
2023-12-10 14:24:13 -0800

    first two deliverables are working

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-function Task() {
+function Task({ text, category, deleteItem }) {
+  const handleDeleteClick = () => {
+    deleteItem(text); // Pass the text to identify the item to be deleted
+  };
   return (
-    <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+    <div className="task" key={text}>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button onClick={handleDeleteClick} className="delete">X</button>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Task from './Task';
 
-function TaskList() {
+function TaskList({ tasks }) {
+  const [items, setItems] = useState(tasks);
+
+  function deleteItem(itemText) {
+    // Filter out the item with the given text
+    const updatedItems = items.filter((item) => item.text !== itemText);
+    setItems(updatedItems);
+  }
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {items.map((task) => (
+        <Task
+          key={task.text}
+          text={task.text}
+          category={task.category}
+          deleteItem={deleteItem}
+        />
+      ))}
     </div>
   );
 }
